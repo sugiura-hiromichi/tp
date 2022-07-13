3845aaf90fa8e62d5907fe864e81c9b1529c1a15
杉浦寛行 <r@rR.local>
2022-07-14 07:59:19 +0900

    prj_tmp for cpp, lua (now)

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+//MF->MakeFile, GI->.gitignore 'LauguageName'->Default Code
+const CPP_MF: &str = "clngp_opt= -std=c++2a -Wall --pedantic-error
+
+run : a.out
+\t./a.out
+
+a.out : main.cpp all.h all.h.pch
+\tclang++ $(clngp_opt) -include all.h $< -o $@
+
+all.h.pch : all.h
+\tclang++ $(clngp_opt) -x c++-header -o $@ $<
+
+clean :
+\trm -f ./a.out
+\trm -f ./all.h.pch
+
+re : clean run
+
+.PHONY : run clean re";
+const CPP_GI: &str = "*.out";
+const CPP: &str = "";
+
+const LUA_MF: &str = "";
+const LUA: &str = "";
+
+fn main() { println!("{CPP_MF}") }
