a1d43531a132ffe3da8d69cacdbe1d4f420ae305
ah-y <sugiura130418@icloud.com>
2022-09-19 18:02:12 +0900

    support ft journal, still bug fix may needed

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,63 @@
-use std::os::unix::process::CommandExt;
+mod templates;
 
 use {
    clap::Parser,
    std::{
       fs,
       io::{self, Write},
+      os::unix::process::CommandExt,
    },
+   templates::*,
 };
-
-//MF->Makefile, GI->.gitignore 'LauguageName'->Default Code
-const CPP_MF: FileBuf = FileBuf {
-   name:    "Makefile",
-   context: b"clngp_opt= -std=c++2a -Wall --pedantic-error
-
-run : a.out
-\t./a.out
-
-a.out : main.cpp all.h all.h.pch
-\tclang++ $(clngp_opt) -include all.h $< -o $@
-
-all.h.pch : all.h
-\tclang++ $(clngp_opt) -x c++-header -o $@ $<
-
-clean :
-\trm -f ./a.out
-\trm -f ./all.h.pch
-
-re : clean run
-
-.PHONY : run clean re",
-};
-
-const CPP_H: FileBuf = FileBuf {
-   name:    "all.h",
-   context: b"#include <cstddef>
-#include <limits>
-#include <climits>
-#include <cfloat>
-#include <cstdint>
-#include <cstdlib>
-#include <new>
-#include <typeinfo>
-#include <exception>
-#include <initializer_list>
-#include <stdexcept>
-#include <cassert>
-#include <cerrno>
-#include <system_error>
-#include <string>
-
-#if __has_include(<string_view>)
-#   include <string_view>
-#endif
-
-#include <array>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <stack>
-#include <iterator>
-#include <algorithm>
-#include <cfenv>
-#include <random>
-#include <numeric>
-#include <cmath>
-#include <iosfwd>
-#include <iostream>
-#include <ios>
-#include <streambuf>
-#include <istream>
-#include <ostream>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
-
-#if __has_include(<filesystem>)
-#   include <filesystem>
-#endif
-
-#include <cstdio>
-#include <cinttypes>
-
-
-#include <regex>
-#include <atomic>
-#include <thread>
-#include <mutex>
-#include <shared_mutex>
-#include <condition_variable>
-#include <future>
-
-using namespace std::literals ;",
-};
-
-const CPP_GI: FileBuf = FileBuf {
-   name:    ".gitignore",
-   context: b"*.out
-*.pch",
-};
-
-const CPP: FileBuf = FileBuf {
-   name:    "main.cpp",
-   context: b"#include \"all.h\"
-using namespace std;
-int main(){}",
-};
-
-const LUA_MF: FileBuf = FileBuf {
-   name:    "Makefile",
-   context: b"run : main.lua
-\tlua main.lua",
-};
-
-const LUA: FileBuf = FileBuf { name: "main.lua", context: b"", };
 
 #[derive(Parser,)]
 #[clap(about)]
 struct TmpPrj {
-   ft:   String,
-   name: String,
+   ///filetype. Currently, cpp, lua and journal are available
+   ft:     String,
+   ///project name
+   name:   String,
+   ///NOTE This option is only valid for journal. Select w: Weekly, m: Monthly, y: Year
+   #[clap(value_parser)]
+   period: char,
 }
 
-struct FileBuf<'a,> {
-   name:    &'a str,
-   context: &'a [u8],
+fn create_files(fstream: Vec<FileBuf,>, prj_name: String,) -> io::Result<(),> {
+   for fb in fstream {
+      let mut f = fs::File::create(format!("{prj_name}/{}", fb.name),)?;
+      f.write(fb.context,)?;
+   }
+   Ok((),)
+}
+
+fn journal(prj_name: String, period: char,) -> io::Result<(),> {
+   let name = match period {
+      'w' => Ok(prj_name.as_str(),),
+      'm' => {
+         fs::create_dir(prj_name,)?;
+         Ok("MONTHLYLOG.md",)
+      },
+      'y' => {
+         fs::create_dir(prj_name,)?;
+         Ok("YEARLOG.md",)
+      },
+      _ => Err(io::Error::new(
+         io::ErrorKind::NotFound,
+         "unknown option for period. period must be 'w' or 'm' or 'y'",
+      ),),
+   }?;
+   let fstream = vec![FileBuf { name, context: JOURNAL, }];
+   create_files(fstream, "./".to_string(),)
 }
 
 fn main() -> io::Result<(),> {
    let tmplt = TmpPrj::parse();
    let prj_name = format!("./{}", &tmplt.name);
    let ft = tmplt.ft;
-   std::fs::create_dir(prj_name.clone(),)?;
+   if "journal".to_string() == ft {
+      return journal(prj_name.clone(), tmplt.period,);
+   }
+   fs::create_dir(prj_name.clone(),)?;
 
    let fstream = if ft == "cpp".to_string() {
       Ok(vec![CPP, CPP_GI, CPP_H, CPP_MF],)
@@ -145,11 +66,7 @@ fn main() -> io::Result<(),> {
    } else {
       Err(io::Error::new(io::ErrorKind::NotFound, "unknown filetype",),)
    }?;
-
-   for fb in fstream {
-      let mut f = fs::File::create(format!("{prj_name}/{}", fb.name),)?;
-      f.write(fb.context,)?;
-   }
+   create_files(fstream, prj_name.clone(),)?;
 
    std::process::Command::new("z",).arg(prj_name,).exec();
    Ok((),)
---
+++ src/templates.rs
@@ -0,0 +1,137 @@
+//! Contain const variables
+
+pub(crate) struct FileBuf<'a,> {
+   pub(crate) name:    &'a str,
+   pub(crate) context: &'a [u8],
+}
+
+//MF->Makefile, GI->.gitignore 'LauguageName'->Default Code
+pub const CPP_MF: FileBuf = FileBuf {
+   name:    "Makefile",
+   context: b"clngp_opt= -std=c++2a -Wall --pedantic-error
+
+run : a.out
+\t./a.out
+
+a.out : main.cpp all.h all.h.pch
+\tclang++ $(clngp_opt) -include all.h $< -o $@
+
+all.h.pch : all.h
+\tclang++ $(clngp_opt) -x c++-header -o $@ $<
+
+clean :
+\trm -f ./a.out
+\trm -f ./all.h.pch
+
+re : clean run
+
+.PHONY : run clean re",
+};
+
+pub const CPP_H: FileBuf = FileBuf {
+   name:    "all.h",
+   context: b"#include <cstddef>
+#include <limits>
+#include <climits>
+#include <cfloat>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+#include <typeinfo>
+#include <exception>
+#include <initializer_list>
+#include <stdexcept>
+#include <cassert>
+#include <cerrno>
+#include <system_error>
+#include <string>
+
+#if __has_include(<string_view>)
+#   include <string_view>
+#endif
+
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <iterator>
+#include <algorithm>
+#include <cfenv>
+#include <random>
+#include <numeric>
+#include <cmath>
+#include <iosfwd>
+#include <iostream>
+#include <ios>
+#include <streambuf>
+#include <istream>
+#include <ostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+
+#if __has_include(<filesystem>)
+#   include <filesystem>
+#endif
+
+#include <cstdio>
+#include <cinttypes>
+
+
+#include <regex>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <condition_variable>
+#include <future>
+
+using namespace std::literals ;",
+};
+
+pub const CPP_GI: FileBuf = FileBuf {
+   name:    ".gitignore",
+   context: b"*.out
+*.pch",
+};
+
+pub const CPP: FileBuf = FileBuf {
+   name:    "main.cpp",
+   context: b"#include \"all.h\"
+using namespace std;
+int main(){}",
+};
+
+pub const LUA_MF: FileBuf = FileBuf {
+   name:    "Makefile",
+   context: b"run : main.lua
+\tlua main.lua",
+};
+
+pub const LUA: FileBuf = FileBuf { name: "main.lua", context: b"", };
+
+pub const JOURNAL: &[u8] = b" # What I want to carry out during 2022?
+
+### Social Require
+
+
+### Progress visualize
+
+
+### Creation
+
+
+### Odd-jobs
+
+---
+
+# Result
+
+";
